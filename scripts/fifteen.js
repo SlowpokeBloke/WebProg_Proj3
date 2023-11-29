3cc2fa7e3ce44380767d75fbec97d6e7154f1022
bang pham <bangapham16@gmail.com>
2023-11-28 19:06:47 -0500

    gameplay music

--- scripts/fifteen.js
+++ scripts/fifteen.js
@@ -1,6 +1,7 @@
 /** effort/time tracking notes (for self ref so i dont forget)
  * base game func - nov 25, 3pm? to 11:30pm mainly - mc
-  */
+ * 
+ */
 
 /** TODO: 
  * game end behavior - homeArr is kept as constant for comparison
@@ -19,58 +20,118 @@
 const homeArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0];
 let tileArr = homeArr;  //array to be shuffled
 
+//setting up timer variables to be used with several functions
+var timer;
+var seconds = 0;
+
+//setting up music
+var music = new Audio('./music/music.mp3');
 /**creates table of 16 cells for gameboard
  * each cell (except 16th) contains tile div
  */
 function createGameBoard(gArr){
     //clears previous gameboard(gTable) if one exists
-    let board = document.getElementById("board");
-    while (board.lastChild) {
+    board = document.getElementById("board");
+    while(board.lastChild){
         board.removeChild(board.lastChild);
     }
 
-    let nRows = Math.floor(Math.sqrt(gArr.length));
-    let nRowCells = nRows;
+    gameSize = gArr.length;     //might be redundant
+    nRows = Math.floor(Math.sqrt(gArr.length));
+    nRowCells = nRows;
 
-    let gTable = document.createElement("table");
+    var gTable = document.createElement("table");
     gTable.classList.add("pTable");
-    let gBody = gTable.createTBody();
+    gBody=gTable.createTBody();
     gBody.classList.add("pBody");
 
-    let tileCtr = 0;
-    let tileNum = gArr[tileCtr];
-    for (let i = 0; i < nRows; i++){
-        let gRow = gBody.insertRow();
+    tileCtr = 0;
+    tileNum = gArr[tileCtr];
+    for( let i = 0; i < nRows; i++){
+        gRow = gBody.insertRow();
         gRow.classList.add("pRow");
-        for(let j = 0; j < nRowCells; j++){
-            let gCell = gRow.insertCell();
+        for( let j=0; j < nRowCells;j++){
+            gCell = gRow.insertCell();
             gCell.classList.add("pCell");
-            if (tileNum < 16 && tileNum !== 0) {
-                gCell.innerHTML = '<div id="t' + tileNum + '" class="tile" title="' + tileCtr + '"> <div class="tileNum">' + tileNum + '</div></div>';
-            } else if (tileNum === 0) {
+            if(tileNum < 16 && tileNum != 0){
+                gCell.innerHTML='<div id="t' + tileNum +'" class="tile" title="' + tileCtr + '"> <div class="tileNum">' + tileNum + '</div></div>' ;
+            } else if(tileNum == 0){
                 gCell.classList.add("empty");
             }
             tileCtr++;
-            tileNum = gArr[tileCtr];
+            tileNum=gArr[tileCtr];
         }
     }
     
     document.getElementById("board").appendChild(gTable);
     addGameHandlers();
+    //play gameplay music
+    music.play();
 }
 
+//setting up timer
 function setTimer(){
-    /** copy func */
+
+    //clear any existing timer on start
+    clearInterval(timer);
+    let seconds = 0;
+
+    timer = setInterval(getSeconds, 1000);
 }
 
+//prints current time to page from inside setTimer function
+function getSeconds(){
+    seconds++;
+    document.getElementById("timer").innerText = "Time Elapsed: " + seconds;
+}
+
+//function to stop time
+function stopTimer() {
+    clearInterval(timer);
+}
+
+function winAnimation(){
+    /*TO DO*/
+}
+
+//stuff that happens when puzzle is finished
+function endGame(){
+    console.log("END GAME CALLED")
+    stopTimer();
+    winAnimation();
+    
+}
+
+//checks if puzzle is solved
+function checkFinish(){
+    var tempTileArr = document.querySelectorAll(".tileNum"); //gets the number on each card
+    for (var i = 0; i <= tempTileArr.length - 1; i++) {
+        let n = i+1;
+
+        //if number not in order, return false (not solved)
+        if (parseInt(tempTileArr[i].textContent) !== n) {
+          return false;
+        }
+      }
+      return true;
+}
+//checks for first shuffle
+let firstShuffle = false;
 /** handles shuffle btn click */
 function handleShuffle(){
     tileArr = shuffle(tileArr);
     createGameBoard(tileArr);
+    setTimer();
+
+    //only play music on first shuffle
+    if (!firstShuffle) {
+        music.play();
+        firstShuffle = true;
+    }
 }
 /** shuffles given array and returns shuffled array */
 function shuffle(gArr){
-    let currIndex = gArr.length, tmpVal, rndIndex;
+    var currIndex = gArr.length, tmpVal, rndIndex;
 
     while(currIndex !== 0){
         rndIndex = Math.floor(Math.random() * currIndex);
@@ -84,95 +145,98 @@ function shuffle(gArr){
 
 /**identifies movable tile and toggles class for css */
 function handleHover(){
-    let currIndex = parseInt(this.title);
+    currIndex=parseInt(this.title);
     
-    let nRowCells = Math.sqrt(tileArr.length); console.log("cells per row: " + nRowCells);
+    var nRowCells = Math.sqrt(tileArr.length); console.log("cells per row: " + nRowCells);
 
     /** checks each neighbor
      * TODO: merge with findEmptyNeighbor func if possible
      */
-    let emptyNeighbor = false; console.log("init val " + emptyNeighbor);
-    if((currIndex + 1) % nRowCells !== 1){   //if not on left side
+    emptyNeighbor = false; console.log("init val " +emptyNeighbor);
+    if((currIndex + 1) % nRowCells != 1){   //if not on left side
         console.log("checking left " + currIndex);
-        if(tileArr[currIndex - 1] === 0){
+        if(tileArr[currIndex-1] == 0){
             emptyNeighbor = true;
             console.log("left empty");
         }
     }
-    if((currIndex + 1) % nRowCells !== 0){   //if not on right side
+    if((currIndex + 1) % nRowCells != 0){   //if not on right side
         console.log("checking right " + (currIndex + 1));
-        if(tileArr[currIndex + 1] === 0){
+        if(tileArr[currIndex + 1] == 0){
             emptyNeighbor = true;
             console.log("right empty");
         }
     }
     if((currIndex + 1) - nRowCells > 0){    //if not on top row
         console.log("checking top");
-        if(tileArr[currIndex - nRowCells] === 0){
+        if(tileArr[currIndex - nRowCells] == 0){
             emptyNeighbor = true;
             console.log("top empty");
         }
     }
     if((currIndex + 1) + nRowCells <= 16){  //if not on bottom row
         console.log("checking bot");
-        if(tileArr[currIndex + nRowCells] === 0){
+        if(tileArr[currIndex + nRowCells] == 0){
             emptyNeighbor = true;
             console.log("bottom empty " + emptyNeighbor);
         }
     }
-    if(emptyNeighbor === true){this.classList.toggle("movablePiece")};
+    if(emptyNeighbor == true){this.classList.toggle("movablePiece")};
 }
 
 /**moves tile to empty space */
 function handleClick(){
     this.classList.remove("movablePiece");
 
-    let currIndex = parseInt(this.title);
-    let emptyNeighborIndex = findEmptyNeighbor(currIndex); console.log(emptyNeighborIndex);
-    if (emptyNeighborIndex !== -1) {
+    currIndex=parseInt(this.title);
+    emptyNeighborIndex = findEmptyNeighbor(currIndex); console.log(emptyNeighborIndex);
+    if(emptyNeighborIndex != -1){
         tileArr[emptyNeighborIndex] = tileArr[currIndex];
         tileArr[currIndex] = 0;
         //console.log(this.parentNode);
-        let currParentNode = this.parentNode;
-        let targetParentNode = document.getElementsByClassName("empty")[0];
+        currParentNode = this.parentNode;
+        targetParentNode = document.getElementsByClassName("empty")[0];
         targetParentNode.classList.remove("empty");
         currParentNode.classList.add("empty");
         currParentNode.removeChild(this);
         targetParentNode.appendChild(this);
         this.title = emptyNeighborIndex;
         targetParentNode.title = currIndex;
+        if (checkFinish()){
+            endGame();
+        }
     }
 }
 
 /**returns index of empty neighbor if one is found, else returns -1 */
 function findEmptyNeighbor(currIndex){
-    let nRowCells = Math.sqrt(tileArr.length);
+    var nRowCells = Math.sqrt(tileArr.length);
 
     /** checks each neighbor */
-    if((currIndex + 1) % nRowCells !== 1){   //if not on left side
+    if((currIndex + 1) % nRowCells != 1){   //if not on left side
         console.log("checking left " + currIndex);
-        if(tileArr[currIndex - 1] === 0){
+        if(tileArr[currIndex-1] == 0){
             console.log("left empty");
             return currIndex - 1;
         }
     }
-    if((currIndex + 1) % nRowCells !== 0){   //if not on right side
+    if((currIndex + 1) % nRowCells != 0){   //if not on right side
         console.log("checking right " + (currIndex + 1));
-        if(tileArr[currIndex + 1] === 0){
+        if(tileArr[currIndex + 1] == 0){
             console.log("right empty");
             return currIndex + 1;
         }
     }
     if((currIndex + 1) - nRowCells > 0){    //if not on top row
         console.log("checking top");
-        if(tileArr[currIndex - nRowCells] === 0){
+        if(tileArr[currIndex - nRowCells] == 0){
             console.log("top empty");
             return currIndex - nRowCells;
         }
     }
     if((currIndex + 1) + nRowCells <= 16){  //if not on bottom row
         console.log("checking bot");
-        if(tileArr[currIndex + nRowCells] === 0){
+        if(tileArr[currIndex + nRowCells] == 0){
             console.log("bottom empty ");
             return currIndex + nRowCells;
         }
@@ -180,14 +244,14 @@ function findEmptyNeighbor(currIndex){
     return -1;  //-1 = no empty neighbor
 }
 
-window.onload = function(){
+window.onload=function(){
     createGameBoard(tileArr);   //initializes board on load in correct order; TODO: lock board before game start
-};
+}
 
 /** adds event listeners on tiles for mouseover and mouseout (hover), click */
 function addGameHandlers(){
-    let tileElemArr = document.querySelectorAll(".tile");
-    for(let i = 0; i < tileElemArr.length; i++){
+    var tileElemArr = document.querySelectorAll(".tile");
+    for(var i = 0; i < tileElemArr.length; i++){
         tileElemArr[i].addEventListener('mouseover', handleHover);
         tileElemArr[i].addEventListener('mouseout', handleHover);
         tileElemArr[i].addEventListener('click', handleClick);
